3ec4ac31f1097381b305e1b8f9d1b77608b0ba4f
zawert879 <zawert23879@gmail.com>
2018-03-31 20:15:24 +0300

    add start

--- start.php
+++ start.php
@@ -13,64 +13,88 @@
 </head>
 <body >
 	<div class="container ">
-	<?php
-	spl_autoload_register(function ($class_name) {
-		include $class_name . '.php';
+		<?php
 
-	});
 
-	include_once "Settings.php";
+		spl_autoload_register(function ($class_name) {
+			include $class_name . '.php';
+
+		});
+	
+		$data = $_POST;
+		$playerRadiant = new Player($data['nameRadiant']);
+		$playerDire = new Player($data['nameDire']);
+
+		unset($data['nameRadiant']);
+		unset($data['nameDire']);
+		
+		for ($i = 1; $i < 11; $i++) {
+
+			if (isset($data['unitRadiant'.$i])) {
+				$className = "Units\\".$data['unitRadiant'.$i];
+				$unitsRadiant[] = new $className();			
+			}
+			if (isset($data['unitDire'.$i])) {
+				$className = "Units\\".$data['unitDire'.$i];
+				$unitsDire[] =new $className();			
+			}
+		}
+		// var_dump($unitsDire);
+		// var_dump($unitsRadiant);
+		$playerRadiant->setUnits($unitsRadiant);
+		$playerDire->setUnits($unitsDire);
 
 
 ///////////////////////////////fight/////////////////////////
 
-	if(count($playerDire->getUnits()) !=2 && count($playerRadiant->getUnits()) !=2){
-		echo '<h2 class="center"> ты как на войну собрался,собери полную команду,придурок!!! </h2>';
-	}else {
-		?>
-		<table class="striped centered">
-			<thead>
-				<tr>
-					<th><?php echo $playerDire->getTeam() ?></th>
-					<th><?php echo $playerRadiant->getTeam() ?></th>
-				</tr>
-			</thead>
-			<tbody><?php 
-
-			while ($playerRadiant->getAllLive() !=0 && $playerDire->getAllLive() !=0) {
-
-				$unitsDire = $playerDire->getUnits();
-				$unitsRadiant = $playerRadiant->getUnits();
-
-				$keyDire = array_rand($unitsDire);
-				$keyRadiant = array_rand($unitsRadiant);
-
-				if($unitsDire[$keyDire]->atack($playerRadiant)){
-					echo '<tr><td colspan="2" class="red white-text" >Юнит игрока '.$playerDire->getTeam()." мертв </td></tr>";
-				}
-				if($unitsRadiant[$keyRadiant]->atack($playerDire)){
-					echo '<tr><td colspan="2" class="red white-text" >Юнит игрока '.$playerRadiant->getTeam()." мертв</td></tr>";
-				}
-				echo "<tr>";
-				echo'<td> '.$playerRadiant->getTeam()."=".$playerRadiant->getAllLive()."</td>";
-				echo"<td>".$playerDire->getTeam()."=".$playerDire->getAllLive()."</td>";
-				echo "</tr>";
-			}
-
+		if(count($playerDire->getUnits()) !=2 && count($playerRadiant->getUnits()) !=2){
+			echo '<h2 class="center"> ты как на войну собрался,собери полную команду,придурок!!! </h2>';
+		}
+		else {
 			?>
+			<table class="striped centered">
+				<thead>
+					<tr>
+						<th><?php echo $playerDire->getTeam() ?></th>
+						<th><?php echo $playerRadiant->getTeam() ?></th>
+					</tr>
+				</thead>
+				<tbody><?php 
 
-		</tbody>
-	</table>
-	<?php 
-	if ($playerDire->getAllLive() != 0) {
-		echo '<h1 class="center">Игрок '.$playerDire->getTeam()." Победил</h1>";
-	}
+				while ($playerRadiant->getAllLive() !=0 && $playerDire->getAllLive() !=0) {
 
-	if ($playerRadiant->getAllLive() != 0) {
-		echo '<h1 class="center"> Игрок '.$playerRadiant->getTeam()." Победил</h1>";
+					$unitsDire = $playerDire->getUnits();
+					$unitsRadiant = $playerRadiant->getUnits();
+
+					$keyDire = array_rand($unitsDire);
+					$keyRadiant = array_rand($unitsRadiant);
+
+					if($unitsDire[$keyDire]->atack($playerRadiant)){
+						echo '<tr><td colspan="2" class="red white-text" >Юнит игрока '.$playerDire->getTeam()." мертв </td></tr>";
+					}
+					if($unitsRadiant[$keyRadiant]->atack($playerDire)){
+						echo '<tr><td colspan="2" class="red white-text" >Юнит игрока '.$playerRadiant->getTeam()." мертв</td></tr>";
+					}
+					echo "<tr>";
+					echo'<td> '.$playerRadiant->getTeam()."=".$playerRadiant->getAllLive()."</td>";
+					echo"<td>".$playerDire->getTeam()."=".$playerDire->getAllLive()."</td>";
+					echo "</tr>";
+				}
+
+				?>
+
+			</tbody>
+		</table>
+		<?php 
+		if ($playerDire->getAllLive() != 0) {
+			echo '<h1 class="center">Игрок '.$playerDire->getTeam()." Победил</h1>";
+		}
+
+		if ($playerRadiant->getAllLive() != 0) {
+			echo '<h1 class="center"> Игрок '.$playerRadiant->getTeam()." Победил</h1>";
+		}
 	}
-}
-?>
+	?>
 </div>
 <div class="container">
 	Copyright © 2018 Cruches and Bikes
